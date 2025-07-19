c31474fb0a95bd20c600ab3256550435ca216707
Ben Wart <benwart@gmail.com>
2025-07-19 06:46:56 -0700

    fix: react 19 cloudflare workaround

--- astro.config.mjs
+++ astro.config.mjs
@@ -56,6 +56,13 @@ export default defineConfig({
 
 	vite: {
 		plugins: [tailwindcss()],
-		optimizeDeps: { include: ['leaflet'] }
+		optimizeDeps: { include: ['leaflet'] },
+		resolve: {
+			// Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+			// Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+			alias: import.meta.env.PROD && {
+				"react-dom/server": "react-dom/server.edge",
+			},
+		},
 	},
 });
