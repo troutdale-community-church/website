09e73516b490b7f50aec5844ba8059e3be60120d
Ben Wart <benwart@gmail.com>
2025-07-19 12:28:31 -0700

    feat: tcc beliefs and distinctives

--- src/config/en/beliefsData.json.ts
+++ src/config/en/beliefsData.json.ts
@@ -5,62 +5,62 @@ export const beliefsData: FaqItem[] = [
     {
         question: 'Bible',
         answer:
-            "We believe that the Bible, consisting of both Old and New Testaments, is God's infallible, inerrant Word, written by men who were uniquely moved by the Holy Spirit, and that it is our sole authority in all matters of faith and practice.* We affirm our belief in the verbal and plenary Inspiration of the Scriptures in their original writings. (II Timothy 3:16, II Peter 1:20-21)",
+            "The Scriptures, comprised of the 66 books of the Old and New Testaments, are the verbally inspired Word of God, are inerrant in the original writings, and are the only standard and authority for belief and behavior for all people in all times and in all places (Psa 19:7; 2Pet 1:19-21). They are the completed canon and revelation of God's will for salvation, and sufficient in all that they speak to (2Ti 3:16-17). We believe in the veracity of God's Word and have deep conviction that His Word is always vindicated which is consistent with the very nature of the Sovereign Lord (Col 2:9). The most complete form of God's special revelation has come through the person of Jesus Christ (Heb 1:1-3).",
     },
     {
-        question: 'God',
+        question: 'Nature of God',
         answer:
-            'We believe in one God, creator of heaven and earth, eternally existing in three persons: God the Father, God the Son, and God the Holy Spirit. (Deuteronomy 6:4, Matthew 28:19).',
+            'There is but one living and true God, Creator and Sustainer of all things, worthy of worship, praise and adoration in his glorious nature and perfect character (Deu 6:4). He exists eternally in three distinct persons—Father, Son and Holy Spirit—coequal and coeternal. The three persons of the godhead each has His own proper attributes (Mat 3:16-17; 28:19).',
     },
     {
         question: 'God the Father',
         answer:
-            'Perfect in holiness, infinite in wisdom, and measureless in power. We rejoice that He concerns Himself mercifully in the affairs of men, that He hears and answers prayer, and that He saves from sin and death all who come to Him through Jesus Christ. Exodus 33:19, Jude 24, 25.',
+            'As the Eternal Creator orders and directs everything according to His own purpose and pleasure (Jhn 5:19-23). He is graciously involved with mankind (Jhn 5:24). He hears and answers prayer (Jhn 16:23-24), and He has designed and established the means of redemption (Eph 1:3-14).',
     },
     {
         question: 'God the Son',
         answer:
-            "Jesus Christ is God's only begotten Son, born of a virgin, sinless in life, who atoned for the sins of the world by His substitutionary death on the cross. We believe in His bodily resurrection, His ascension into heaven, His high priestly intercession for His people, His lordship* and His personal visible, return to the world according to His promise. (Colossians 1:13-18, Matthew 1:22-23, Hebrews 4:14-16).",
+            "Jesus Christ, has always existed as the eternal Son of the Father (Jhn 1:1, 3; 8:58; 17:5; Phl 2:4-8). As the second person of the Godhead He is active at the creation (Col 1:15-16; Heb 1:2-3). He was conceived by the Holy Spirit and born of the virgin Mary (Isa 7:14; 9:6-7; Mat 1:18, 24-25; Luk 1:34-35; Jhn 1:14). As true God and true man, His substitutionary death is the permanent sacrifice and payment for our sins, the only remedy by which man's sin could be removed and God's justice preserved (Mat 20:28; Rom 3:23-26; 1Co 15:3; Col 2:13-15; 2Jn 2:2). His death also represents His victory over Satan (Col 1:13-14; 2:15; 1Jn 3:8; Rev 20:10). He was raised bodily and ascended into heaven (Luk 25:50-51; Acts 1:9; 1Co 15:3-8; Eph 4:8-10). He presently intercedes for us as our High Priest (Heb 3:1) and Advocate (1Jn 2:1) until His personal return to this earth to establish His Kingdom (1Th 4:13-18; Rev 20; Heb 7:23-25).",
     },
     {
         question: 'God the Holy Spirit',
         answer:
-            'convicts the world of sin, of righteousness and of judgment. His works in the believer include (but are not limited to): salvation, baptizing into the body of Christ, indwelling, sealing eternally, progressive sanctification,* gifting, filling, teaching, enabling; and glorifying. (John 16:8-13, Ephesians 1:13-14, I Corinthians 12:12-14).',
+            "Third person of the Trinity, is active in creation (Gen 1:2), in the inspired writing of the Old and New Testaments (Jhn 16:13-15; 2Pet 1:20-21), in Jesus' earthly ministry (Luk 4:1, 10:21), and in the work of salvation. Presently, He convicts the world of sin, righteousness, and judgment (Jhn 16:8-11). He gives new life to those who place faith in Christ's atoning death and resurrection (Col 1:12-14; 2Co 5:17). The Holy Spirit glorifies Jesus Christ and implements Christ's work of redemption (Jhn 16:14; Rom 8:23). He regenerates (Tit 3:5), baptizes (1Co 12:13), seals (2Co 1:22; 5:5; Eph 1:13), gifts (1Co 12:1-11; 1Pet 4:10), and indwells all believers at conversion (Rom 8:9; Eph 3:16), progressively sanctifying (Phl 1:6), and securing them in Christ forever (Rom 8:16-17). The Spirit fills (Eph 5:18), leads (Psa 143:10; Rom 8:14), comforts (Acts 9:31), and intercedes for believers (Rom 8:26-27), empowering them for godly living and service (2Ti 1:7). His presence is evident by the fruit of the Spirit (Gal 5:22-23) and the building up of believers into the body of Christ, the Church (1Co 12:13-20)."
     },
     {
-        question: 'Man',
+        question: 'Nature of Satan',
         answer:
-            'We believe that man was created in the image of God and was originally without sin. Because of the sin of Adam and Eve, all men are now sinners both by nature and by choice. Man is unable to remedy his own lost condition. Every believer is capable, under God, of deciding for himself in the matters of faith and life.* (Genesis 1:26-27, Romans 5:12, John 3:16).',
+            "Satan is a personal being, a created angel who sought to be “like the Most High” and thus fell from glory and became the author of sin (Isa 14:12-17; Ezek 28:11-19; Rev 12:9-11). He provoked the fall of man (Gen 3:1-6, 14-15) and seeks constantly to counterfeit the works and truth of God (Ex 7:8-13; Mat 4:1-11; 2Co 4:3-4; Eph 2:1-3). As the archenemy of God, he is destined to eternal punishment in the Lake of Fire (Rev 20:10).",
     },
     {
-        question: 'Salvation',
+        question: 'Nature of Man',
         answer:
-            "We believe that salvation is God's gift to man in the person of Jesus Christ. Sinful man is saved from the eternal penalty of sin as a result of receiving Jesus Christ by faith, as his/her Savior and Lord of life. (Ephesians 2:8-9, John 3:16, Romans 6:23).",
+            "Man was created directly in the image of God, male and female (Gen 1:26-27; 2:7; 9:6), in a state of innocence with the ability not to sin (Gen 2:16-17, 25), to enjoy an intimate relationship with God (Gen 5:22-24; Psa 139:13-14) and to fulfill His purpose on earth (Gen 2:15; Prv 16:9). Man is contingent; that is, his creation was not necessary to God's being as only God is necessary (Acts 17:24-28). God created man for His own purposes (Isa 55:8-9), especially for God's glory (Rom 15:6). But man failed to trust God's goodness and obey God's stipulation and thus earned condemnation and death for himself and all his progeny (Gen 3:1-24; Rom 1:18-32). The fall also subjected creation to corruption (Rom 8:19-22). All men are sinners, by nature and practice, and are spiritually dead - separated from God forever, apart from salvation through Christ (Rom 5:12-21; 6:23; Eph. 2:1-10)."
     },
     {
-        question: 'The Church',
+        question: "The Work of Christ",
         answer:
-            "We believe that the church is the body of Christ, which is made up of every born-again believer since Pentecost regardless of race, sex, or national origin. The local church is an organized body of immersed* believers* voluntarily associated together for the worship of God, the fellowship of believers, the proclamation of the Gospel of Christ throughout the world, and the observance of the Biblical ordinances (believer's baptism and the Lord's Supper). The Scriptural officers of the Church are described in I Timothy 3. The local church is independent and autonomous* and is free from all outside ecclesiastical* or political* interference. (2 Timothy 2:19, Matthew 28:18-20, Ephesians 4:4-16, Acts 4:19).",
+            "Jesus Christ died for our sins as the only qualified sacrificial substitute (Rom 6:10; Heb 7:26-27; 9:11-14). His atoning death fully accomplished the salvation of His elect and, at the same time, paid the penalty for sin (1Ti 2:5-6; 1Jo 2:1-2), making possible man's forgiveness, apart from any human merit, works, or ritual (1Jn 1:5-10). All who repent and believe solely in Christ for salvation are born again by the Holy Spirit (Jhn 3:16; 5:24; 6:37-40; 14:6) and are made spiritually alive (Rom 8:11; 1Jo 5:11-12). Those saved in Christ are the children of God eternally (Joh 1:12; Gal 3:26). They are empowered by the Holy Spirit to practice righteous living, and they can again enjoy an intimate relationship with God through Christ (Eph 2:4-10; 3:16-17; Rom 15:13).",
     },
     {
-        question: 'Christian Practice',
+        question: "The Church",
         answer:
-            'We believe Christians, individually and collectively, are salt and light in society (Matthew 5:13-16). In a Christ like spirit, they oppose greed, selfishness and vice; they promote truth, justice and peace; they aid the needy and preserve the dignity of people of all races and conditions (Hebrews 13:5; Luke 9:23; Titus 2:12; Philippians 4:8-9, 1 John 3:16-17; James 2:1-4, 14-17). We affirm the family as the basic unit of society and seek to preserve its integrity and stability (Genesis 2:21-25; Ephesians 6:1-4). Marriage is defined by God as being between one man (natural born) and one woman (natural born) (Genesis 1:27-28, 2:24; Leviticus 18:22; Romans 1:26-27).',
+            "True believers in Jesus Christ belong to the universal Body of Christ, comprised of all who have been saved through faith in Christ (1Co 12:12-13, 27). Every believer is directed by God's Word to be baptized (immersed) in water as a public profession of salvation, identifying with Christ's death, burial, and resurrection, in order to become an active participant in a local church (Mat 28:19-20; Rom 6:1-11). Believers are not to forsake the physical assembling with other believers (Acts 2:41-47; Heb 10:23-25). Believers are to fellowship regularly around the Lord's Supper (also called Communion) as instructed in the Word (Mat 26:26-29; Mar 14:22-25; Luk 22:14-23;1Co 11:23-32). Believers are to give of their time, labor, gifts, talents, and treasure for the building up of the Body of Christ (Acts 2:41-47; 1Co 12:4-11; Rom 12:3-8; 1Pe 2:4-5, 9-10). The church corporately (and its individual members) is responsible to Christ as the Head (Eph 5:23). The local church is autonomous, free from any external authority or control (Acts 6:1-6).",
     },
     {
-        question: 'Angels',
+        question: 'Marriage and Family',
         answer:
-            'We believe God created an order of spiritual beings called angels to serve Him and do His will (Psalm 103:20; Colossians l:16). The holy angels are obedient spirits ministering to the heirs of salvation and glorifying God (Revelation 5:11-12).',
+            "God desires human flourishing (Gen 1:26-28; 2:23-24; 9:1, 44). In God's design for humanity, life begins at fertilization (Psa 51:5; 139:13-16; Jer 1:5; Mat 1:34-35; Luk 1:31, 41). He established the institution of family through the union of one chromosomal man and one chromosomal woman in a marriage covenant for life (Mat 19:3-12; Mar 10:1-12). Marriage is to picture the relationship between Jesus Christ and His Church (Eph 5:22-33; Rev 19:7, 21:2, 9; 22:17). He ordained the continuance of humanity through the procreation and discipleship of children within the marriage union (Eph 5:22-6:4; Col 3:18-21; 1Ti 3:2-5).",
     },
     {
-        question: 'Satan',
+        question: "Death and Resurrection",
         answer:
-            'We believe that Satan is a person, a fallen angel, the author of sin, and the cause of the fall of man. He is the open and declared enemy of God and man. He will be eternally punished in the Lake of Fire. (Isaiah 14:12-17, Ezekiel 28:11-17, Revelation 20:7-10).',
+            "God alone is eternal. He has granted immortality to man. At one's physical death, the material and immaterial are divided. No one ceases to exist at death. (Ecc 12:7; Jas 2:26; Luk 23:42-43; Phi 1:21-23; Luk 16:19-31).<ol><li>At death, the spirit of the believer in Jesus Christ passes immediately and consciously into Christ's presence, there to await the resurrection, when spirit and body will be reunited (2Co 5:6-9; Heb 9:27). This will happen at Christ's imminent coming to receive the church (Mat 24:36-41; Mar 13:32-37; 1Th 4:13-18; Rev 3:10). All believers of all ages will remain forever with the Lord in this glorified state (Jhn 14:3; 1Co 15:50-57). Every believer, furthermore, will also appear before Christ to give an accounting and receive rewards (2Co 5:10; Rom 14:10-12).</li><li>Every unbeliever will remain after death in conscious misery until the final judgment at the Great White Throne (Rev 20:11-12), when the spirit reunites with the resurrected body (Rev 20:13) to be cast into the Lake of Fire and punished with everlasting torment from the presence of the Lord (Mat 25:41-46; Luk 12:4-5; Rev 20:14-15).</li></ol>",
     },
     {
         question: 'Last Things',
         answer:
-            "We believe in the sudden, visible return of the Lord Jesus Christ and in His millennial Kingdom (Mark 13:32-37; Titus 2:13; Revelation 20:2-6). He will return with His saints from heaven with power and great glory, on a day known only to God (Matthew 24:30, 36; 25:31; Titus 2:13). We believe that those who have accepted God's gift of salvation through faith in Jesus Christ, at the point of physical death, enter into God's eternal glory (John 10:28; Romans 8:38-39; 2 Corinthians 5:8; Hebrews 9:27). They shall be resurrected and transformed to His body of glory, and they shall receive their inheritance of glory in the Kingdom of their Father (Isaiah 26:19; Daniel 12:2; John 5:25-29; Acts 24:14-15; 1 Corinthians 6:14; Philippians 3:21; 1 Thessalonians 4:13-17; Revelation 20:5-6). They shall be made fully blessed in the presence and service of God, whom they shall see and enjoy throughout the ages (1 Thessalonians 4:17; Revelation 22:3-5). We believe that all unsaved persons, at the point of physical death, are destined for eternal damnation (Hebrews 9:27, Matthew 25:44-46). They will be resurrected, separated from the righteous, judged, and cast into an existence of eternal punishment where they will be tormented day and night forever and ever when death and Hades are cast into the Lake of Fire (Daniel 12:2; Matthew 25:44-46; John 5:25-29; Revelation 20:11-15, 21:8). We believe it is imperative for the Church to work and wait in sober watchfulness, that it may be found ready at His coming (Matthew 24:42-44; 25:13; Mark 13:33-37; Hebrews 10:25).",
+            "On a day known only to God (Mat 24:36; Acts 1:7), Christ will personally return to earth in a glorious, bodily, visible coming, accompanied by all His saints, to complete His work as Messiah, to subdue His enemies, to judge all people, and to reign on the earth with His saints (Dan 7:25-27; Rev 19:11-20). Christ will sit on his Great White Throne and judge the living and the dead (Rev 20:11). Everyone whose name is not found in the book of life will be thrown into hell (Rev 20:15) where they will suffer forever together with Satan and his angels (Rev 20:10). Those whose names are found in the book of life will be welcomed into everlasting blessing in the presence of God in the new heavens and the new earth (Isa 35:4-10; Eze 37:27; Rev 21-22).",
     },
 ];
 
